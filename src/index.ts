f0d4c833a6eefdcb8ca379d1fbafed615e6a8c4a
vinci <szqq602@163.com>
2023-04-24 01:39:31 +0800

    写法优化一下

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import configs from "./configs.json";
+const list = Object.values(configs);
+
+// console.log("list.length", list.length);
 
 export default function detectPatterns(name: `${string}.bit`): Set<string> {
   const prefix = name.slice(0, -4);
   const results = new Set<string>();
-  for (const config of Object.values(configs)) {
+  for (const config of list) {
     // 排除掉 Rare4D
-    if (config.names.includes(prefix) && config.name.en != "Rare4D") {
+    if (config.name.en != "Rare4D" && config.names.includes(prefix)) {
       results.add(config.name.en);
     }
   }
